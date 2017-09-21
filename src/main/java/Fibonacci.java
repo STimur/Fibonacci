de429b3850d079317ce6359fde179002715a3a95
Timur <timur.salakhetdinov@gmail.com>
2017-09-21 03:54:55 +0300

    clean code

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,6 +2,6 @@ public class Fibonacci {
     public static int of(int n) {
         if (n <= 1)
             return n;
-        return of(n-2) + of(n-1);
+        return of(n - 1) + of(n - 2);
     }
 }
