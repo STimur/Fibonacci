d4bf05e95c5948d47e33e89050d8f3e6f460a572
Timur <timur.salakhetdinov@gmail.com>
2017-09-21 05:01:53 +0300

    clean code

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,14 +1,11 @@
 public class Fibonacci {
     public static int of(int n) {
-        return of(0, 1, n);
-    }
-
-    private static int of(int a, int b, int n) {
-        while (n != 0) {
+        int a = 0;
+        int b = 1;
+        for (; n != 0; n--) {
             int c = a + b;
             a = b;
             b = c;
-            n--;
         }
         return a;
     }
